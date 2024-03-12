bd7fa10a9553884cc0b7620e73416d2fc51f1c82
dev-zuo <i@zuoguoqing.com>
2024-03-13 00:43:31 +0800

    feat: 添加新增配置逻辑

--- configList/components/ConfigList.vue
+++ configList/components/ConfigList.vue
@@ -7,10 +7,12 @@
       </div>
       <el-table v-loading="loading" :data="tableDataObject.list">
         <el-table-column prop="name" label="名称" />
-        <el-table-column prop="id" label="ID" />
-        <el-table-column prop="id" label="ID">
+        <el-table-column prop="bid" label="配置ID" />
+        <el-table-column prop="config" label="配置" />
+        <el-table-column prop="createDate" label="创建日期" />
+        <!-- <el-table-column prop="id" label="ID">
           <template #default="scope">{{ scope.row.type }}</template>
-        </el-table-column>
+        </el-table-column> -->
         <el-table-column prop="operate" label="操作" width="180">
           <template #default="scope">
             <el-button type="primary" text @click="handleEdit(scope.row)"
@@ -36,15 +38,39 @@
       </div>
     </div>
   </div>
+  <el-dialog v-model="dialogFormVisible" title="创建低代码配置" width="500">
+    <el-form :model="form">
+      <el-form-item label="配置名称" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off" />
+      </el-form-item>
+      <!-- <el-form-item label="Zones" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="Please select a zone">
+          <el-option label="Zone No.1" value="shanghai" />
+          <el-option label="Zone No.2" value="beijing" />
+        </el-select>
+      </el-form-item> -->
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="saveConfig"> 确定 </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive } from "vue";
 import { usePagination } from "../composition/use-pagination";
 import { ElMessageBox } from "element-plus";
 import { ElMessage } from "element-plus";
+import apiServer from "../utils/axios";
+import { log } from "console";
 
 const loading = ref(false);
-
+const dialogFormVisible = ref(false);
+const form = reactive({
+  name: "",
+});
 async function findPage() {
   loading.value = true;
   const payload = {
@@ -54,8 +80,12 @@ async function findPage() {
   try {
     console.log(JSON.stringify(payload, null, 2));
     await new Promise((r) => setTimeout(r, 500));
-    // const res = await apiGetList(payload)
-    const res = { list: [{}, {}], total: 100 };
+    const res: any = await apiServer.get("/configList/list", {
+      params: payload,
+    });
+    console.log("res", res);
+
+    // const res = { list: [{}, {}], total: 100 };
     tableDataObject.list = res.list;
     tableDataObject.total = res.total;
   } catch (e) {
@@ -104,6 +134,14 @@ async function handleDelete(row: any) {
 }
 
 function addConfig() {
+  dialogFormVisible.value = true;
+}
+
+function saveConfig() {
+  gotoConfigPage();
+}
+
+function gotoConfigPage() {
   function generateId() {
     let chars =
       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -115,9 +153,13 @@ function addConfig() {
   }
   // 生成一个 16 位的随机 ID
   let randomId = generateId();
+  const name = encodeURIComponent(form.name);
   console.log(randomId);
   //   window.open(`http://127.0.0.1:3000?bid=${randomId}`, "_blank");
-  window.open(`http://design.formily.top?bid=${randomId}`, "_blank");
+  window.open(
+    `${import.meta.env.VITE_DESIGN_URL}?bid=${randomId}&name=${name}`,
+    "_blank"
+  );
 }
 </script>
 
